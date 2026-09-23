a36b1220a37f4bb24f3d9581990da7182a152f22
knottnt <trevor.knottnerus@gmail.com>
2026-09-23 15:11:11 -0700

    Remove ARN gate for AccessPointPolicy read in sdkFind

--- pkg/resource/access_point/sdk.go
+++ pkg/resource/access_point/sdk.go
@@ -146,23 +146,21 @@ func (rm *resourceManager) sdkFind(
 	}
 
 	rm.setStatusDefaults(ko)
-	if ko.Status.ACKResourceMetadata != nil && ko.Status.ACKResourceMetadata.ARN != nil {
-		policyInput := &svcsdk.GetAccessPointPolicyInput{
-			AccountId: r.ko.Spec.AccountID,
-			Name:      r.ko.Spec.Name,
-		}
-		policyResp, policyErr := rm.sdkapi.GetAccessPointPolicy(ctx, policyInput)
-		rm.metrics.RecordAPICall("READ_ONE", "GetAccessPointPolicy", policyErr)
-		if policyErr != nil {
-			var awsErr smithy.APIError
-			if errors.As(policyErr, &awsErr) && awsErr.ErrorCode() == "NoSuchAccessPointPolicy" {
-				ko.Spec.Policy = nil
-			} else {
-				return nil, policyErr
-			}
+	policyInput := &svcsdk.GetAccessPointPolicyInput{
+		AccountId: r.ko.Spec.AccountID,
+		Name:      r.ko.Spec.Name,
+	}
+	policyResp, policyErr := rm.sdkapi.GetAccessPointPolicy(ctx, policyInput)
+	rm.metrics.RecordAPICall("READ_ONE", "GetAccessPointPolicy", policyErr)
+	if policyErr != nil {
+		var awsErr smithy.APIError
+		if errors.As(policyErr, &awsErr) && awsErr.ErrorCode() == "NoSuchAccessPointPolicy" {
+			ko.Spec.Policy = nil
 		} else {
-			ko.Spec.Policy = policyResp.Policy
+			return nil, policyErr
 		}
+	} else {
+		ko.Spec.Policy = policyResp.Policy
 	}
 
 	return &resource{ko}, nil
